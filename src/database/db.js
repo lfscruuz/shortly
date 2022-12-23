1316940b5495e101394a22477fea59ceaf4b0408
luis <lfscruz2001@gmail.com>
2022-12-23 13:04:26 -0300

    .env consertado

--- src/database/db.js
+++ src/database/db.js
@@ -1,8 +1,9 @@
 import pkg from "pg";
-
 const {Pool} = pkg;
+import dotenv from "dotenv";
+dotenv.config();
 
 export const connectionDB = new Pool({
     connectionString: process.env.DATABASE_URL,
-    ssl: {rejectUnauthorized: false}
+    ssl: true
 })
