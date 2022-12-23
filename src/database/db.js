e9e1920c233f4ed6b06461418a53f0e45d2c108d
luis <lfscruz2001@gmail.com>
2022-12-23 11:58:42 -0300

    connection string adicionada

--- src/database/db.js
+++ src/database/db.js
@@ -3,9 +3,6 @@ import pkg from "pg";
 const {Pool} = pkg;
 
 export const connectionDB = new Pool({
-    host: "localhost",
-    port: 5432,
-    user: "postgres",
-    password: "395112",
-    database: "shortly"
+    connectionString: process.env.DATABASE_URL,
+    ssl: true
 })
